113d446fb95878feceb12d71194524ed89b22b14
M.Haris Khan <hkhan@cedrus.com>
2023-07-18 15:00:15 +0500

    Feat;IntegerArray

---
+++ IntegerArray/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static bool ContainsDuplicate(int[] nums)
+    {
+        HashSet<int> set = new HashSet<int>();
+
+        foreach (int num in nums)
+        {
+            if (set.Contains(num))
+            {
+                return true;
+            }
+            set.Add(num);
+        }
+
+        return false;
+    }
+
+    static void Main(string[] args)
+    {
+        int[] nums = { 1, 2, 3, 1 };
+        bool hasDuplicates = ContainsDuplicate(nums);
+
+        Console.WriteLine(hasDuplicates ? "true" : "false");
+        Console.ReadLine();
+    }
+}
